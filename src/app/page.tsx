f57575c4a1b0cda3ca5b29899c1bd319de74fc87
Fahmi Achmad <fahmiachmadf13@gmail.com>
2024-01-10 23:52:28 +0700

    feat: update dependecies

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Maintenance from "@/components/maintenance";
 export default function Home() {
   return (
     <main className="flex justify-center items-center h-screen flex-col">
-      <Maintenance />
+      {/* <Maintenance /> */}
       <div className="text-xl">Website is under maintenance</div>
     </main>
   );
